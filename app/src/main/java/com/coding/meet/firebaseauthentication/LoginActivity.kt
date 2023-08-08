13b48ae6f25272cf2854daa6aabae05740ebad2a
coding meet <codingmeet26@gmail.com>
2023-08-08 14:07:04 +0530

    google sign in with firebase authentication

--- app/src/main/java/com/coding/meet/firebaseauthentication/LoginActivity.kt
+++ app/src/main/java/com/coding/meet/firebaseauthentication/LoginActivity.kt
@@ -8,13 +8,18 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Button
 import android.widget.LinearLayout
-import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
+    private val googleSignInRequestCode = 234
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -88,5 +93,57 @@ class LoginActivity : AppCompatActivity() {
             }
 
         }
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+        val googleSignInBtn = findViewById<SignInButton>(R.id.googleSignInBtn)
+        googleSignInBtn.setOnClickListener {
+            if (isConnected(this)){
+                val signInIntent =  googleSignInClient.signInIntent
+                startActivityForResult(signInIntent,googleSignInRequestCode)
+            }else{
+                longToastShow("No Internet Connection!")
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode){
+            googleSignInRequestCode -> {
+                try {
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                    val account = task.getResult(ApiException::class.java)
+                    firebaseAuthWithGoogle(account)
+
+                }catch (e:ApiException){
+                    e.printStackTrace()
+                    e.message?.let { longToastShow(it) }
+                }
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+            .addOnSuccessListener {
+                longToastShow("Login Successful")
+                val mainIntent = Intent(this,MainActivity::class.java)
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(mainIntent)
+                finish()
+            }
+            .addOnFailureListener {
+                it.printStackTrace()
+                it.message?.let { it1 -> longToastShow(it1) }
+            }
     }
 }
